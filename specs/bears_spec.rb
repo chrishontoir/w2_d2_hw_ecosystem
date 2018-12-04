a9c93943aa4a4553e1b72d836dc5ded7d6440f26
user <user@users-MacBook-Pro.local>
2018-12-04 18:56:33 +0000

    Created and tested a method for a bear to take a fish from the river.

--- specs/bears_spec.rb
+++ specs/bears_spec.rb
@@ -2,6 +2,7 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../bears')
 require_relative('../fish')
+require_relative('../river')
 
 class BearsTest < MiniTest::Test
 
@@ -26,19 +27,39 @@ class BearsTest < MiniTest::Test
   end
 
   def test_add_contents_to_stomach
-    @fish1 = Fish.new("Salmon")
-    @bears.add_content_to_stomach(@fish1)
+    fish1 = Fish.new("Salmon")
+    @bears.add_content_to_stomach(fish1)
     assert_equal(1, @bears.stomach_count)
   end
 
   def test_remove_contents_from_stomach
-    @fish1 = Fish.new("Salmon")
-    @fish2 = Fish.new("Tuna")
-    @fish3 = Fish.new("Mackerel")
-    @bears.add_content_to_stomach(@fish1)
-    @bears.add_content_to_stomach(@fish2)
-    @bears.add_content_to_stomach(@fish3)
-    @bears.remove_content_from_stomach(@fish1)
+    fish1 = Fish.new("Salmon")
+    fish2 = Fish.new("Tuna")
+    fish3 = Fish.new("Mackerel")
+    @bears.add_content_to_stomach(fish1)
+    @bears.add_content_to_stomach(fish2)
+    @bears.add_content_to_stomach(fish3)
+    @bears.remove_content_from_stomach(fish1)
     assert_equal(2, @bears.stomach_count)
+  end
+
+  def test_take_fish_from_river
+    fish1 = Fish.new("Salmon")
+    fish2 = Fish.new("Tuna")
+    fish3 = Fish.new("Mackerel")
+    fish4 = Fish.new("Trout")
+    river = River.new("Amazon", [])
+
+    river.add_fish_to_river(fish1)
+    river.add_fish_to_river(fish2)
+    river.add_fish_to_river(fish3)
+    river.add_fish_to_river(fish4)
+
+    river.remove_fish_from_river(fish3)
+    @bears.add_content_to_stomach(fish3)
+
+    assert_equal(3, river.fish_count)
+    assert_equal(1, @bears.stomach_count)
+
   end
 end
