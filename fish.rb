22fac7cb7d90100dc31cb80b2a360fe65fd09b81
user <user@users-MacBook-Pro.local>
2018-12-04 18:11:05 +0000

    Fish class created and tested with getters.

---
+++ fish.rb
@@ -0,0 +1,12 @@
+class Fish
+
+  def initialize(fish_type)
+    @fish_type = fish_type
+  end
+
+  def get_fish_type
+    return @fish_type
+  end
+
+
+end
---
+++ specs/fish_spec.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../fish')
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish = Fish.new("Salmon")
+  end
+
+  def test_return_fish_type
+    assert_equal("Salmon", @fish.get_fish_type)
+  end
+
+
+end
